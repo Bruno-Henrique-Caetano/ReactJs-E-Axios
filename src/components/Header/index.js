07eafbc0d02ac785f6027ec6809c5b6fbf54520e
Bruno-Henrique-Caetano <56415776+Bruno-Henrique-Caetano@users.noreply.github.com>
2019-10-25 20:34:50 -0300

    tutorial in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,8 @@ import './style.css';
 
  const Header = () => (
     <header id="main-header">JSHunt</header> 
+    <h1> Application in reactjs to acess api data </h1>
+    <p> This header is a component to fixed in top of the page</p>
  );
 
  export default Header;
